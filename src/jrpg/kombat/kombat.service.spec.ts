3b7ef730361973d86d2396df98a3eb4ea2b508b9
Luis Arcos Muñoz <arcos.lwm@gmail.com>
2023-08-18 13:50:15 -0400

    adds unitTests for kombatService

--- src/jrpg/kombat/kombat.service.spec.ts
+++ src/jrpg/kombat/kombat.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { KombatService } from './kombat.service';
+import { BadRequestException } from '@nestjs/common';
+import { CreateKombatDto } from '../dto/request/create-kombat.dto';
 
 describe('KombatService', () => {
   let service: KombatService;
@@ -15,4 +17,79 @@ describe('KombatService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('checkKombat thrown Exception', () => {
+    const mockInvalidCreateKombatDto: CreateKombatDto = {
+      player1: {
+        movimientos: ['D', 'DSD', 'S', 'DSD', 'SD'],
+        golpes: ['K', 'P', '', 'K', 'P'],
+      },
+      player2: {
+        movimientos: ['SA', 'SA', 'SA', 'ASA', 'SA'],
+        golpes: [''],
+      },
+    };
+
+    expect(() => {
+      service.checkKombat(mockInvalidCreateKombatDto);
+    }).toThrow(BadRequestException);
+  });
+
+  it('createKombat shoult define fighters', () => {
+    const mockCreateKombatDto: CreateKombatDto = {
+      player1: {
+        movimientos: ['D', 'DSD', 'S', 'DSD', 'SD'],
+        golpes: ['K', 'P', '', 'K', 'P'],
+      },
+      player2: {
+        movimientos: ['SA', 'SA', 'SA', 'ASA', 'SA'],
+        golpes: ['', 'P', '', 'K', 'P'],
+      },
+    };
+
+    service.createKombat(mockCreateKombatDto);
+    expect(service.attackingFighter).toBeDefined();
+    expect(service.attackedFighter).toBeDefined();
+  });
+
+  it('confrontToTheDeath shoult call checkKombat and createKombat', async () => {
+    const mockCreateKombatDto: CreateKombatDto = {
+      player1: {
+        movimientos: ['D', 'DSD', 'S', 'DSD', 'SD'],
+        golpes: ['K', 'P', '', 'K', 'P'],
+      },
+      player2: {
+        movimientos: ['SA', 'SA', 'SA', 'ASA', 'SA'],
+        golpes: ['', 'P', '', 'K', 'P'],
+      },
+    };
+
+    const checkKombatSpy = jest.spyOn(service, 'checkKombat');
+    const createKombatSpy = jest.spyOn(service, 'createKombat');
+
+    service.confrontToTheDeath(mockCreateKombatDto);
+
+    expect(checkKombatSpy).toHaveBeenCalledWith(mockCreateKombatDto);
+    expect(createKombatSpy).toHaveBeenCalledWith(mockCreateKombatDto);
+  });
+
+  it('confrontToTheDeath shoult provide a valid response', async () => {
+    const mockCreateKombatDto: CreateKombatDto = {
+      player1: {
+        movimientos: ['D', 'DSD', 'S', 'DSD', 'SD'],
+        golpes: ['K', 'P', '', 'K', 'P'],
+      },
+      player2: {
+        movimientos: ['SA', 'SA', 'SA', 'ASA', 'SA'],
+        golpes: ['', 'P', '', 'K', 'P'],
+      },
+    };
+
+    const kombatResponseDto = service.confrontToTheDeath(mockCreateKombatDto);
+
+    expect(kombatResponseDto).toHaveProperty('result');
+    expect(kombatResponseDto).toHaveProperty('kombatHistory');
+    expect(kombatResponseDto).toHaveProperty('fighters');
+  });
+
 });
